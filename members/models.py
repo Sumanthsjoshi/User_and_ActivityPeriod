03d9a7d49927d9200ec7b8a73fb3831da9b1d860
Sumanth Joshi <sumanthsjoshi@gmail.com>
2020-07-03 18:55:55 +0530

    Created members app and corresponding User and ActivityPeriod models

--- members/models.py
+++ members/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+    user_id = models.CharField(max_length=10)
     real_name = models.CharField(max_length=100)
     tz = models.CharField(max_length=200)
+    def __str__(self):
+        return self.user_id + ' : ' + self.real_name
 
 class ActivityPeriod(models.Model):
+    user = models.ForeignKey(User, models.CASCADE)
     start_time = models.DateTimeField('start time')
     end_time = models.DateTimeField('end time')
